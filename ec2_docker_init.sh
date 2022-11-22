3bc89d9c757b905c8d1b772628135f6801524412
nexxyb <nexxyb@gmail.com>
2022-11-22 13:49:21 +0100

    first commit

--- ec2_docker_init.sh
+++ ec2_docker_init.sh
@@ -10,8 +10,6 @@ sudo apt install docker-ce
 sudo usermod -aG docker ubuntu
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo gpasswd -a $USER docker
-newgrp docker
 sudo apt install unzip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
@@ -24,4 +22,6 @@ sudo apt-get update
 sudo apt-get install -y kubectl
 curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 sudo install minikube-linux-amd64 /usr/local/bin/minikube
+sudo gpasswd -a $USER docker
+newgrp docker
 minikube start
