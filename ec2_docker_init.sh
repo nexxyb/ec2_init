0456a3a2c5c9a2a84b7d9fb01235e0b12222953e
nexxyb <nexxyb@gmail.com>
2023-01-17 16:21:04 +0100

    updated

--- ec2_docker_init.sh
+++ ec2_docker_init.sh
@@ -18,8 +18,9 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 aws ecr get-login-password --region us-east-1
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 813095119948.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 532146782045.dkr.ecr.us-east-1.amazonaws.com
 
 
 sudo gpasswd -a $USER docker
 newgrp docker
+sudo apt update
